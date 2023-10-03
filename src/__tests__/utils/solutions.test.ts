5f1b9245865b7897468a19b27e4f865f5746657d
intaniger <intaniger@gmail.com>
2023-10-04 02:39:15 +0700

    chore(utils): :white_check_mark: added preflight testcases

--- src/__tests__/utils/solutions.test.ts
+++ src/__tests__/utils/solutions.test.ts
@@ -9,6 +9,18 @@ describe('sol-path', () => {
     'sfw/query_results-2023-10-03_84741',
     'sfw/query_results-2023-10-03_90248',
     'sfw/query_results-2023-10-03_91144',
+    'sfw/query_results-2023-10-04_11322',
+    'sfw/query_results-2023-10-04_12002',
+    'sfw/query_results-2023-10-04_12350',
+    'sfw/query_results-2023-10-04_12633',
+    'sfw/query_results-2023-10-04_13011',
+    'sfw/query_results-2023-10-04_13829',
+    'sfw/query_results-2023-10-04_14422',
+    'sfw/query_results-2023-10-04_20141',
+    'sfw/query_results-2023-10-04_20653',
+    'sfw/query_results-2023-10-04_21107',
+    'sfw/query_results-2023-10-04_21345',
+    'sfw/query_results-2023-10-04_21837',
   ])('Preflight check - %s', (filename) => {
     expect(openSync(toSystemPath(filename), 'r')).toEqual(expect.any(Number))
   })
