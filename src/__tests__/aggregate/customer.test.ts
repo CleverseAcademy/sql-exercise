761e59678a52ba40e5ae85f4442c3b8287d8e92a
intaniger <intaniger@gmail.com>
2023-10-09 22:40:26 +0700

    test(aggregate): :memo: added title to each test case

--- src/__tests__/aggregate/customer.test.ts
+++ src/__tests__/aggregate/customer.test.ts
@@ -22,14 +22,50 @@ afterAll((done) => {
 
 describe('Customer Aggregate query', () => {
   test.each<[string, SolutionFilePath, ReturnType<QueryGenerator>]>([
-    [`Q1`, 'aggregate/query_results-2023-10-04_45202', q1],
-    [`Q2`, 'aggregate/query_results-2023-10-04_45829', q2],
-    [`Q3`, 'aggregate/query_results-2023-10-04_45901', q3],
-    [`Q4`, 'aggregate/query_results-2023-10-04_45923', q4],
-    [`Q5`, 'aggregate/query_results-2023-10-04_45941', q5],
-    [`Q6`, 'aggregate/query_results-2023-10-04_50004', q6],
-    [`Q7`, 'aggregate/query_results-2023-10-04_50113', q7],
-    [`Q8`, 'aggregate/query_results-2023-10-04_50131', q8],
-    [`Q9`, 'aggregate/query_results-2023-10-04_50150', q9],
+    [
+      `Q1: Calculate the average year-to-date payment of every customer.`,
+      'aggregate/query_results-2023-10-04_45202',
+      q1,
+    ],
+    [
+      `Q2: Calculate the average customer year-to-date payment for each state`,
+      'aggregate/query_results-2023-10-04_45829',
+      q2,
+    ],
+    [
+      `Q3: Calculate the maximum of the family discount, where their last name begins with CALLY or ANTI`,
+      'aggregate/query_results-2023-10-04_45901',
+      q3,
+    ],
+    [
+      `Q4: For families whose last name starts with OUGHT or EINGEIN, determine the family minimum balance.`,
+      'aggregate/query_results-2023-10-04_45923',
+      q4,
+    ],
+    [
+      `Q5: To determine which consumers have good and bad credit, count a number of them.`,
+      'aggregate/query_results-2023-10-04_45941',
+      q5,
+    ],
+    [
+      `Q6: Report the customer discount percentage distribution`,
+      'aggregate/query_results-2023-10-04_50004',
+      q6,
+    ],
+    [
+      `Q7: Add up all customer year-to-date payments, grouping by state`,
+      'aggregate/query_results-2023-10-04_50113',
+      q7,
+    ],
+    [
+      `Q8: Report all states in which the total customer year-to-date payment is greater than $360,000`,
+      'aggregate/query_results-2023-10-04_50131',
+      q8,
+    ],
+    [
+      `Q9: Summarize districts' total of customer year-to-date payment`,
+      'aggregate/query_results-2023-10-04_50150',
+      q9,
+    ],
   ])('%s - %s', unorderedRowTest(clientPtr))
 })
