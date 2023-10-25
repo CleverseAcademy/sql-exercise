5b3729c2e6d5dd8f3806e2ca20a420dcaa166133
intaniger <intaniger@gmail.com>
2023-10-26 01:22:22 +0700

    test: :monocle_face: more readable row testing

--- src/__tests__/utils/helpers.row.test.ts
+++ src/__tests__/utils/helpers.row.test.ts
@@ -9,21 +9,31 @@ export interface ClientPtr {
   client: Client | null;
 }
 
+const mapObjectToHumanReadableRow = (value: SQLReturnRowInterface) =>
+  Object.entries(value)
+    .sort(([k1], [k2]) => (k1 < k2 ? -1 : 1))
+    .map(([k, v]) => `${k}=${v}`)
+    .join(", ");
+
 export const unorderedRowTest =
   (ptr: ClientPtr) =>
   async (
     _: string,
     sol: SolutionFilePath,
-    intermediateQuery: ReturnType<QueryGenerator>,
+    intermediateQuery: ReturnType<QueryGenerator>
   ) => {
     if (ptr.client === null) throw new Error("Receiving null pointer");
     const solution: SQLReturnRowInterface[] = JSON.parse(
-      readFileSync(toSystemPath(sol)).toString(),
+      readFileSync(toSystemPath(sol)).toString()
     );
-    const queryResult = await intermediateQuery(ptr.client);
+    const queryResult = (await intermediateQuery(ptr.client)).map(
+      mapObjectToHumanReadableRow
+    );
 
-    // console.log(`Expected # of rows: ${solution.length}`)
-    solution.forEach((row) => expect(queryResult).toContainEqual(row));
+    expect(queryResult).toHaveLength(solution.length);
+    solution
+      .map(mapObjectToHumanReadableRow)
+      .forEach((row) => expect(queryResult).toContainEqual(row));
   };
 
 export const orderedRowTest =
@@ -31,13 +41,18 @@ export const orderedRowTest =
   async (
     _: string,
     sol: SolutionFilePath,
-    intermediateQuery: ReturnType<QueryGenerator>,
+    intermediateQuery: ReturnType<QueryGenerator>
   ) => {
     if (ptr.client === null) throw new Error("Receiving null pointer");
     const solution: SQLReturnRowInterface[] = JSON.parse(
-      readFileSync(toSystemPath(sol)).toString(),
+      readFileSync(toSystemPath(sol)).toString()
     );
-    const queryResult = await intermediateQuery(ptr.client);
+    const queryResult = (await intermediateQuery(ptr.client)).map(
+      mapObjectToHumanReadableRow
+    );
 
-    solution.forEach((row, index) => expect(row).toEqual(queryResult[index]));
+    expect(queryResult).toHaveLength(solution.length);
+    solution
+      .map(mapObjectToHumanReadableRow)
+      .forEach((row, index) => expect(row).toEqual(queryResult[index]));
   };
